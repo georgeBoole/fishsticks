141aef9fe79bf216473bab911ba9fd4ad177104c
Michael Sobczak <michaelsobczak54@gmail.com>
2013-06-23 12:48:40 -0400

    Started to give main.js the functionality found in dev scripts

--- impact/lib/game/main.js
+++ impact/lib/game/main.js
@@ -7,16 +7,25 @@ ig.module(
 )
 .defines(function(){
 
-MyGame = ig.Game.extend({
-	
-	// Load a font
+// wrap the builtins, eventually move this out
+bind = ig.input.bind;
+key = function(k) {
+	return eval('ig.KEY.' + k.toUpperCase());
+}
+
+AngryMiner = ig.Game.extend({
+
 	font: new ig.Font( 'media/04b03.font.png' ),
 	
-	
 	init: function() {
-		// Initialize your game here; bind keys etc.
+		var name = prompt('Enter your name:');
+		this.initInput();
 	},
-	
+	initInput: function() {
+		bind(key('mouse1'), 'shoot');
+		bind(key('space'), 'cart');
+		ig.input.initMouse();
+	},
 	update: function() {
 		// Update all entities and backgroundMaps
 		this.parent();
@@ -40,6 +49,6 @@ MyGame = ig.Game.extend({
 
 // Start the Game with 60fps, a resolution of 320x240, scaled
 // up by a factor of 2
-ig.main( '#canvas', MyGame, 60, 320, 240, 2 );
+ig.main( '#canvas', AngryMiner, 60, 320, 240, 2 );
 
 });
