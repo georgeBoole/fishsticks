33d5820e7a627e4ce91547de14083ab470c2e299
Michael Sobczak <michaelsobczak54@gmail.com>
2013-06-24 01:51:08 -0400

    changed a ton of stuff, general maintenance

--- impact/lib/game/entities/player.js
+++ impact/lib/game/entities/player.js
@@ -1,13 +1,15 @@
 ig.module(
 	'game.entities.player'
 ).requires(
-	'impact.entity'
+	'impact.entity',
+	'impact.font'
 ).defines(function() {
 	EntityPlayer = ig.Entity.extend({
 		animSheet: new ig.AnimationSheet('media/playerSprite.png',32,32),
 		size: {x:32,y:32},
 		name: 'John Doe',
 		slot: undefined,
+		kills: 0,
 		init: function(x,y,settings){
 			this.parent(x,y,settings);
 			this.addAnim('idle',1,[0]);
@@ -17,6 +19,8 @@ ig.module(
 		},
 		draw: function() {
 			this.parent();
+			ig.game.big_font.draw(this.name, this.pos.x + this.size.x / 2, this.pos.y - (this.size.y * 2.0), ig.Font.ALIGN.CENTER);
+
 		},
 		update: function() {
 			this.parent();
