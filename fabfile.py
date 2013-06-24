9e9cbd938e01f0817052b94f399d329805d87086
Michael Sobczak <michaelsobczak54@gmail.com>
2013-06-24 18:10:43 -0400

    added some plumbing

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,26 @@
 
 from fabric.api import run, env, local, lcd
-from sh import git, apt
 
 PROJECT_TITLE = 'Angry Miner'
 
 env.hosts = ['michael@elendil.hawksites.com']
+
+def install_deps():
+	dependencies = {
+		'apt-get -y install':[
+			'php5-cli',
+			'python-dev',
+			'python-virtualenv'
+		],
+		'pip install':[
+			'Jinja2'
+		]
+	}
+	for installer, packages in dependencies:
+		for pkg in packages:
+			local('%s %s' % (installer, pkg))
+	
+
 
 def bake():
 	with lcd('impact/tools'):
