e1288d50e655579059d6b63af8c749a3553de385
Julian <juliantoker@gmail.com>
2013-07-08 02:15:51 -0400

    Changed entity name

---
+++ impact/lib/game/entities/track.js
@@ -0,0 +1,24 @@
+ig.module(
+	'game.entities.track'
+).requires(
+	'impact.entity'
+).defines(function() {
+	EntityPipe = ig.Entity.extend({
+		animSheet: new ig.AnimationSheet('media/track.png', 1000, 163),
+		size: {x: 1000,y: 163},
+		maxVel: {x:1000,y:1000},
+		init: function(x,y,settings) {
+			this.parent(x,y,settings);
+			this.addAnim('idle',1,[0]);
+		},
+		kill: function() {
+			this.parent();
+		},
+		draw: function() {
+			this.parent();
+		},
+		update: function() {
+			this.parent();
+		}
+	});
+});
